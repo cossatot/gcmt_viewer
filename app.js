6407d1588a71ed896a9f11fe49f7d1ac07f8780c
Caitlin Hennessy <caitlin@cs.uoregon.edu>
2016-08-12 20:23:03 -0700

    Return a piece of data from database

--- app.js
+++ app.js
@@ -6,12 +6,15 @@ var url = "mongodb://admin:segfault@ds145355.mlab.com:45355/gcmt_dev";
 MongoClient.connect(url, function(err, db) {
 	if (err) throw err;
 	var router = function(req, res) {
-		res.writeHead(200, {
-			"Content-Type": "application/json",
-			"Access-Control-Allow-Origin" : "*"
+		db.collection("caitlintestcollection").findOne({}, function(err, result) {
+			res.writeHead(200, {
+				"Content-Type": "application/json",
+				"Access-Control-Allow-Origin" : "*"
+			});
+			res.write(JSON.stringify({data : "pineapple"}));
+			res.write(JSON.stringify(result));
+			res.end();
 		});
-		res.write(JSON.stringify({data : "pineapple"}));
-		res.end();
 	};
 	var server = http.createServer(router);
 	server.listen(3000);
