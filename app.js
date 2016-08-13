580d454ebc7bde0676dee5ab8a2140511c54f265
Caitlin Hennessy <caitlin@cs.uoregon.edu>
2016-08-12 19:05:45 -0700

    Add mongo connection to app.js

--- app.js
+++ app.js
@@ -1,14 +1,18 @@
 var http = require("http");
+var url = require("url");
+var MongoClient = require("mongodb").MongoClient;
+var url = "mongodb://admin:segfault@ds145355.mlab.com:45355/gcmt_dev";
 
-var router = function(req, res) {
-	res.writeHead(200, {
-		"Content-Type": "application/json",
-		"Access-Control-Allow-Origin" : "*"
-	});
-	res.write(JSON.stringify({data : "pineapple"}));
-	res.end();
-}
-
-var server = http.createServer(router);
-
-server.listen(3000);
+MongoClient.connect(url, function(err, db) {
+	if (err) throw err;
+	var router = function(req, res) {
+		res.writeHead(200, {
+			"Content-Type": "application/json",
+			"Access-Control-Allow-Origin" : "*"
+		});
+		res.write(JSON.stringify({data : "pineapple"}));
+		res.end();
+	};
+	var server = http.createServer(router);
+	server.listen(3000);
+});
--- initial_load.js
+++ initial_load.js
@@ -8,12 +8,6 @@ MongoClient.connect(url, function(err, db) {
     console.log("ERROR");
     throw err;
   }
-  console.log("here");
-  /**
-  var filenames = ["ATA", "HimaTibetMap", "gcmt_4-_icons", "gcmt_5-55_icons",
-  "gcmt_5-6_icons", "gcmt_55-6_icons", "gcmt_6-7_icons", "gcmt_7+_icons",
-  "gcmt_icons", "gcmt_icons_55plus", "gcmt_icons_60plus", "pb_steps",
-  "plate_bounds_bird02"];**/
 
   var filenames = ["ATA", "gcmt_icons_reduced", "HimaTibetMap", "plate_bounds_bird02"];
   
@@ -38,17 +32,4 @@ MongoClient.connect(url, function(err, db) {
       });
     });
   })
-
-  /**
-  filenames.map((filename) => {
-    var json = JSON.parse(fs.readFileSync("./data/" + filename + ".geojson", "utf8"));
-    var features = json.features;
-    console.log("Starting to insert " + filename);
-    db.collection("caitlintestcollection").insertMany(features, (err, res) => {
-      if (err) throw err;
-      var totalTime = process.hrtime()[0] - startTime;
-      console.log("All entries from " + filename + " inserted ok. Elapsed time is " + totalTime + " seconds.");
-    });
-  });
-**/
 });
