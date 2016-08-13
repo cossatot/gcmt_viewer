692ecfc4c916ce0e08217bbd11a6b701d93c2137
Caitlin Hennessy <caitlin@cs.uoregon.edu>
2016-08-12 17:37:53 -0700

    Rename initial load file

--- initial_load.js
+++ initial_load.js
@@ -9,13 +9,37 @@ MongoClient.connect(url, function(err, db) {
     throw err;
   }
   console.log("here");
+  /**
   var filenames = ["ATA", "HimaTibetMap", "gcmt_4-_icons", "gcmt_5-55_icons",
   "gcmt_5-6_icons", "gcmt_55-6_icons", "gcmt_6-7_icons", "gcmt_7+_icons",
   "gcmt_icons", "gcmt_icons_55plus", "gcmt_icons_60plus", "pb_steps",
-  "plate_bounds_bird02"];
+  "plate_bounds_bird02"];**/
+
+  var filenames = ["ATA", "gcmt_icons_reduced", "HimaTibetMap", "plate_bounds_bird02"];
   
   var startTime = process.hrtime()[0];
 
+  db.collection("caitlinstestcollection").deleteMany({}, (err, res) => {
+    console.log("Starting inserts");
+    filenames.map((filename) => {
+      var json = JSON.parse(fs.readFileSync("./data/" + filename + ".geojson", "utf8"));
+      var features = json.features;
+      console.log("Starting to insert " + filename);
+      db.collection("caitlintestcollection").insertMany(features, (err, res) => {
+        if (err) throw err;
+        var totalTime = process.hrtime()[0] - startTime;
+        console.log("All entries from " + filename + " inserted ok. Elapsed time is " + totalTime + " seconds.");
+        /** Don't know why this part hangs
+        db.collection("caitlintestcollection").count(function(err, res) {
+            console.log("Total count is " + count);
+            console.log("Insertion rate is " + (count/totalTime) + " records per second.");
+        });
+        **/
+      });
+    });
+  })
+
+  /**
   filenames.map((filename) => {
     var json = JSON.parse(fs.readFileSync("./data/" + filename + ".geojson", "utf8"));
     var features = json.features;
@@ -24,12 +48,7 @@ MongoClient.connect(url, function(err, db) {
       if (err) throw err;
       var totalTime = process.hrtime()[0] - startTime;
       console.log("All entries from " + filename + " inserted ok. Elapsed time is " + totalTime + " seconds.");
-      /** Don't know why this part hangs
-      db.collection("caitlintestcollection").count(function(err, res) {
-          console.log("Total count is " + count);
-          console.log("Insertion rate is " + (count/totalTime) + " records per second.");
-      });
-      **/
     });
   });
+**/
 });
