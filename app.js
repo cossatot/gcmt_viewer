39194d57edef2837c7f110499577f66b5968a087
Caitlin Hennessy <caitlin@cs.uoregon.edu>
2016-08-13 20:10:55 -0700

    Stuff

--- app.js
+++ app.js
@@ -1,20 +1,43 @@
 var MongoClient = require("mongodb").MongoClient;
 var mongoUrl = "mongodb://admin:segfault@ds145355.mlab.com:45355/gcmt_dev";
 var express = require("express");
+var bodyParser = require("body-parser");
 var app = express();
+app.use(bodyParser.json());
 
 MongoClient.connect(mongoUrl, function(err, db) {
 	if (err) throw err;
 
+	app.all('/', function(req, res, next) {
+  		res.header("Access-Control-Allow-Origin", "*");
+  		//res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  		next();
+ 	});
+
 	app.get("/", function(req, res) {
+
+		console.log("In / endpoint!");
+		console.log(req.body);
+
 		db.collection("caitlintestcollection").findOne({}, function(err, result) {
+			if (err) {
+				console.log("ERROR IN DB QUERY");
+				throw err;
+			}
+			console.log("Made database call; result = ");
+			console.log(result);
+			console.log(JSON.stringify(result));
+			/**
 			res.writeHead(200, {
 				"Content-Type": "application/json",
 				"Access-Control-Allow-Origin" : "*"
 			});
-			res.write(JSON.stringify({data : "pineapple"}));
-			res.write(JSON.stringify(result));
-			res.end();
+			**/
+			//res.write(JSON.stringify({data : "pineapple"}));
+			//res.write(JSON.stringify(result));
+			res.json(result);
+			//res.end();
+			console.log("Sent http response");
 		});
 	});
 
