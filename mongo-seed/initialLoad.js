a2058d9337fd755889ed9cad299b957101ad8572
Caitlin Hennessy <caitlin@cs.uoregon.edu>
2016-09-19 19:17:04 -0700

    Update initial load script

--- mongo-seed/initialLoad.js
+++ mongo-seed/initialLoad.js
@@ -8,29 +8,22 @@ MongoClient.connect(url, function(err, db) {
     console.log("ERROR");
     throw err;
   }
-  console.log("here");
-  var filenames = ["ATA", "HimaTibetMap", "plate_bounds_bird02"];
   
   var startTime = process.hrtime()[0];
 
-  /**
-  var specialjson = JSON.parse(fs.readFileSync("./data/init.geojson", "utf8"));
-  console.log("starting to insert init file");
-  db.collection("caitlintestcollection").insertMany(specialjson, (err, res) => {
+  var quakesjson = JSON.parse(fs.readFileSync("./data/quakes_init.geojson", "utf8"));
+  console.log("Starting to insert quakes file");
+  db.collection("caitlinquakes").insertMany(quakesjson, (err, res) => {
     if (err) throw err;
     var totalTime = process.hrtime()[0] - startTime;
-    console.log("All entries from init inserted OK");
+    console.log("All entries from quakes inserted OK");
   });
-  **/
-  
-  filenames.map((filename) => {
-    var json = JSON.parse(fs.readFileSync("./data/" + filename + ".geojson", "utf8"));
-    var features = json.features;
-    console.log("Starting to insert " + filename);
-    db.collection("caitlintestcollection").insertMany(features, (err, res) => {
-      if (err) throw err;
-      var totalTime = process.hrtime()[0] - startTime;
-      console.log("All entries from " + filename + " inserted ok. Elapsed time is " + totalTime + " seconds.");
-    });
+
+  var faultsjson = JSON.parse(fs.readFileSync("./data/faults_init.geojson", "utf8"));
+  console.log("Starting to insert faults file");
+  db.collection("caitlinfaults").insertMany(faultsjson, (err, res) => {
+    if (err) throw err;
+    var totalTime = process.hrtime()[0] - startTime;
+    console.log("All entries from faults inserted OK");
   });
 });
